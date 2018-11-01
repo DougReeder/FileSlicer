30f92b33f405fdd0367dc51ad9282a19511a8ccd
P. Douglas Reeder <reeder.29@gmail.com>
2018-11-01 01:06:17 -0400

    Calculates SHA-256 of each slice

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ FileSlicer
 -----
 
 A test of using Blob.slice() on files, to check memory use.
+
+Calculates the SHA-256 hash of each slice
+(but not the hash of the whole file, as the SubtleCrypto API doesn't support progressive calculation).
--- fileSlicer.js
+++ fileSlicer.js
@@ -10,19 +10,61 @@ document.addEventListener("DOMContentLoaded", function(event) {
         let actualSliceSize = Number.POSITIVE_INFINITY;
         let position = 0;
         let counter = 0;
-        while (actualSliceSize > 0) {
-            let slice = selectedFile.slice(position, position + sliceSize);
+        let slice = selectedFile.slice(position, position + sliceSize);
 
+        fileReader.onload = function (evt) {
+            let arrayBuffer = evt.target.result;
+            let msg = "slice #" + counter + "   position:" + position +
+                    "   arrayBuffer.byteLength:" + arrayBuffer.byteLength;
             actualSliceSize = slice.size;
-            let msg = "slice #" + counter + "   position:" + position + "   slice.size:" + slice.size;
-            console.log(msg);
             ++counter;
 
-            let div = document.createElement('div');
-            div.innerText = msg;
-            document.body.appendChild(div);
+            if (actualSliceSize > 0) {
+                crypto.subtle.digest("SHA-256", arrayBuffer).then(function (hash) {
+                    msg += "   SHA-256: " + hex(hash);
+                    output(msg);
 
-            position += actualSliceSize;
-        }
+                    position += actualSliceSize;
+                    slice = selectedFile.slice(position, position + sliceSize);
+                    fileReader.readAsArrayBuffer(slice);
+                });
+            } else {
+                output("done");
+            }
+
+        };
+        fileReader.onerror = function (err) {
+            console.error(err);
+        };
+        fileReader.readAsArrayBuffer(slice);
+
     });
+
+    function hex(buffer) {
+        var hexCodes = [];
+        var view = new DataView(buffer);
+        for (var i = 0; i < view.byteLength; i += 4) {
+            // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
+            var value = view.getUint32(i)
+            // toString(16) will give the hex representation of the number without padding
+            var stringValue = value.toString(16)
+            // We use concatenation and slice for padding
+            var padding = '00000000'
+            var paddedValue = (padding + stringValue).slice(-padding.length)
+            hexCodes.push(paddedValue);
+        }
+
+        // Join all the hex strings into one
+        return hexCodes.join("");
+    }
+
+    function output(msg) {
+        console.log(msg);
+
+        let div = document.createElement('div');
+        div.innerText = msg;
+        document.body.appendChild(div);
+
+    }
+
 });
