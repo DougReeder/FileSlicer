31a49e73696e5bf102b157818739e6afe34a6064
P. Douglas Reeder <reeder.29@gmail.com>
2018-11-02 21:10:17 -0400

    Adds MD5 computation of slice and whole file.

--- fileSlicer.js
+++ fileSlicer.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
         console.log("selectedFile:", selectedFile);
         let sliceSize = parseInt(document.getElementById('sliceSize').value, 10);
 
+        let md5whole = new SparkMD5.ArrayBuffer();
+        let md5slice = new SparkMD5.ArrayBuffer();
         let fileReader = new FileReader();
         let actualSliceSize = Number.POSITIVE_INFINITY;
         let position = 0;
@@ -20,43 +22,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
             ++counter;
 
             if (actualSliceSize > 0) {
-                crypto.subtle.digest("SHA-256", arrayBuffer).then(function (hash) {
-                    msg += "   SHA-256: " + hex(hash);
-                    output(msg);
+                md5whole.append(arrayBuffer);
 
-                    position += actualSliceSize;
-                    slice = selectedFile.slice(position, position + sliceSize);
-                    fileReader.readAsArrayBuffer(slice);
-                });
+                msg += "   MD5: " + SparkMD5.ArrayBuffer.hash(arrayBuffer);
+                output(msg);
+
+                position += actualSliceSize;
+                slice = selectedFile.slice(position, position + sliceSize);
+                fileReader.readAsArrayBuffer(slice);
             } else {
-                output("done");
+                let hash = md5whole.end();
+                output("MD5: " + hash);
             }
 
         };
         fileReader.onerror = function (err) {
-            console.error(err);
+            output(err);
         };
         fileReader.readAsArrayBuffer(slice);
 
     });
-
-    function hex(buffer) {
-        var hexCodes = [];
-        var view = new DataView(buffer);
-        for (var i = 0; i < view.byteLength; i += 4) {
-            // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
-            var value = view.getUint32(i)
-            // toString(16) will give the hex representation of the number without padding
-            var stringValue = value.toString(16)
-            // We use concatenation and slice for padding
-            var padding = '00000000'
-            var paddedValue = (padding + stringValue).slice(-padding.length)
-            hexCodes.push(paddedValue);
-        }
-
-        // Join all the hex strings into one
-        return hexCodes.join("");
-    }
 
     function output(msg) {
         console.log(msg);
